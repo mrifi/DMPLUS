602006c42b9340cf43e8c0cb68127631c6a198d1
mohamed rifi <mohaa.rifii@gmail.com>
2021-04-05 11:22:40 +0200

    commit logo

---
+++ PROJECT/Ubicacion_csv.py
@@ -0,0 +1,6 @@
+class Ubicacion_csv:
+    def __init__(self):
+        self.file_csv = "C:\\Users\\mohaa\\OneDrive\\Escritorio\\DMPLUS\\PROJECT\\diabetes.csv"
+        
+    def __str__(self):
+        return str(self.file_csv)
